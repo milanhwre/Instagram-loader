1ea9dbf2871cd1eda9229f3f0e6fc610d95920d3
Milan Hwre <milan123nisum@gmail.com>
2025-02-04 11:38:38 +0545

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ html_template = """
 </head>
 <body>
     <div class="container">
-        <h2>Instagram Message Sender</h2>
+        <h2> Anish Instagram Message Sender</h2>
 
         {% if error %}
         <div class="error">{{ error }}</div>
